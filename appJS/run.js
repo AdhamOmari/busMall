3241eeb0490c7dae428364803a9fb61917534ae1
AdhamOmari <adhamomarics@gmail.com>
2021-05-17 22:32:50 +0300

    creat lab

--- appJS/run.js
+++ appJS/run.js
@@ -1,7 +1,13 @@
 'use strict';
 let attempts = 0;
-let maxAttmpts = 10;
-
+let maxAttmpts = 0;
+let userEnter = prompt('maxAttmpts')
+if (userEnter > 25) {
+    maxAttmpts = 25;
+}
+else if (userEnter < 25) {
+    maxAttmpts = userEnter;
+}
 
 let mall = [];
 let nameOfImge = [];
@@ -19,9 +25,9 @@ function busMall(busName) {
 }
 
 let mallImages = [
-    'bag.jpg','banana.jpg','bathroom.jpg','boots.jpg','breakfast.jpg','bubblegum.jpg','chair.jpg','cthulhu.jpg',
-    'dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg','sweep.png','tauntaun.jpg'
-    ,'unicorn.jpg','water-can.jpg','wine-glass.jpg'];
+    'bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg',
+    'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg'
+    , 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg'];
 
 for (let i = 0; i < mallImages.length; i++) {
 
@@ -73,90 +79,90 @@ lastImg.addEventListener('click', userClick);
 function userClick(event) {
     attempts++;
 
-
-
     if (attempts <= maxAttmpts) {
 
         if (event.target.id === 'firstImg') {
             mall[leftImgIndex].clicks++;
-        } else if (event.target.id === 'secondImg') {
+        }
+        else if (event.target.id === 'secondImg') {
 
             mall[rightImgIndex].clicks++;
         }
+
         else if (event.target.id === 'lastImg') {
             mall[lastImgIndex].clicks++;
         }
 
         renderImg();
-    }
-
-    let buttom = document.getElementById('go');
-    buttom.addEventListener('click', resulatS)
-
-    function resulatS(event) {
-
-        let ulEl = document.getElementById('results');
-        let LiEl;
-
-        for (let j = 0; j < mall.length; j++) {
-            LiEl = document.createElement('li');
-            ulEl.appendChild(LiEl);
-            LiEl.textContent = `${mall[j].busName} had ${mall[j].clicks} votes, and was seen ${mall[j].views} times`
-            busClicks.push(mall[j].clicks);
-            busviews.push(mall[j].views)
-        }
-
-        firstImg.removeEventListener('click', userClick);
-        secondImg.removeEventListener('click', userClick);
-        lastImg.removeEventListener('click', userClick);
-
-        chartrender();
 
     }
-
 }
 
-function chartrender() {
+let buttom = document.getElementById('go');
+buttom.addEventListener('click', resulatS)
 
+function resulatS(event) {
 
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: nameOfImge,
-            datasets: [{
-                label: '# of views',
-                data: busviews,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                  
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                   
-                ],
-                borderWidth: 1
-            }, {
-                label: '# of clicks',
-                data: busClicks,
-                backgroundColor: [
-                    
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
+    let ulEl = document.getElementById('results');
+    let LiEl;
+    for (let j = 0; j <= mall.length; j++) {
+        LiEl = document.createElement('li');
+        ulEl.appendChild(LiEl);
+        LiEl.textContent = `${mall[j].busName} had ${mall[j].clicks} votes, and was seen ${mall[j].views} times`
+        busClicks.push(mall[j].clicks);
+        busviews.push(mall[j].views)
+        banana had 3 votes, and was seen 5 times.
+    }
 
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
+    firstImg.removeEventListener('click', userClick);
+    secondImg.removeEventListener('click', userClick);
+    lastImg.removeEventListener('click', userClick);
 }
+chartrender();
+
+
+
+// function chartrender() {
+
+
+//     var ctx = document.getElementById('myChart').getContext('2d');
+//     var myChart = new Chart(ctx, {
+//         type: 'bar',
+//         data: {
+//             labels: nameOfImge,
+//             datasets: [{
+//                 label: '# of views',
+//                 data: busviews,
+//                 backgroundColor: [
+//                     'rgba(255, 99, 132, 0.2)',
+
+//                 ],
+//                 borderColor: [
+//                     'rgba(255, 99, 132, 1)',
+
+//                 ],
+//                 borderWidth: 1
+//             }, {
+//                 label: '# of clicks',
+//                 data: busClicks,
+//                 backgroundColor: [
+
+//                     'rgba(255, 159, 64, 0.2)'
+//                 ],
+//                 borderColor: [
+
+//                     'rgba(255, 159, 64, 1)'
+//                 ],
+//                 borderWidth: 1
+//             }]
+
+//         },
+//         options: {
+//             scales: {
+//                 y: {
+//                     beginAtZero: true
+//                 }
+//             }
+//         }
+//     });
+// }
